bcb99a8289f826dd6a851d7a45f03c72f3390387
charlie <the_charlie_daly@hotmail.co.uk>
2018-03-23 10:23:24 +0800

    More stuff. bit of a mess.

--- public/site.js
+++ public/site.js
@@ -1,35 +1,11 @@
-const launchAuth = (containerCss) => {
-    var uiConfig = {
-        signInSuccessUrl: '/',
-        signInOptions: [
-          // Leave the lines as is for the providers you want to offer your users.
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-          firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-          firebase.auth.GithubAuthProvider.PROVIDER_ID,
-          firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          firebase.auth.PhoneAuthProvider.PROVIDER_ID
-        ],
-        // Terms of service url.
-        tosUrl: 'http://test.com'
-      };
-
-    // Initialize the FirebaseUI Widget using Firebase.
-    var ui = new firebaseui.auth.AuthUI(firebase.auth());
-    ui.start("#auth-container", uiConfig);
-}
-
 const launchPage = () => {
+    const db = firebase.app().database();
     var VM = function() {
         const self = this;
-        const db = firebase.app().database();
         const url = window.location.pathname;
         
-        self.message = ko.observable("");
-        self.name = ko.observable('');
         self.isLoading = ko.observable(true);
-        self.charities = ko.observable([]);
-
+        self.data = ko.observable();
         self.init = () => {
             let id = url.replace('/', '');
             console.info("Loading " + id);
@@ -40,13 +16,12 @@ const launchPage = () => {
                     window.location.href = "/404.html";
                 }
 
-                self.message(val.message);
-                self.name(val.name);
-                self.charities(val.charities);
+                self.data(val);
 
-                console.log(snap.val());
+                console.log(val);
             })
-            .catch(e => console.error(e));
+            .catch(e => console.error(e))
+            .then(() => self.isLoading(false));
         }
     }
 
@@ -55,79 +30,132 @@ const launchPage = () => {
     vm.init();
 }
 
-ko.components.register('charity-dontate-card', {
-    viewModel: function(params) {
-        const self = this;
+var AuthVM = function(fb) {
+    const self = this;
+    
+    self.isLoggedIn = ko.observable(false);
+    self.user = ko.observable();
+    self.pageId = ko.observable();
 
-        this.charityId = params.charityId;
-        this.amount = params.amount;
+    fb.auth().onAuthStateChanged(user => {
+        if (user) {
+            console.info('Logged in.');
+            self.isLoggedIn(true);
+            self.user(user);
+            self.initUser();
+        }
+        else {
+            console.info('Logged out.')
+            self.isLoggedIn(false);
+            self.user();
+        }
+    });
 
-        self.isLoading = ko.observable(true);
-        self.imageUrl = ko.observable('');
-        self.siteUrl = ko.observable('');
-        self.title = ko.observable('');
-        self.description = ko.observable('');
+    self.initUser = () => {
+        console.info('Start init user');
+        var ref = fb.database().ref('user/' + value.uid);
+        
+        ref.once('value', snap => {
+            var val = snap.val()
+            if (!val) {
+                var newVal = {
+                    
+                }
+            }
+            console.info('Received user page id ' + pageId);
+            self.pageId(pageId);
+        })
+    }
 
-        this.subFive = function() {
-            var next = this.amount() + 5;
-            this.amount(next).bind(this);
-        }
-        this.subTen = function() {
-            var next = this.amount() + 10;
-            this.amount(next).bind(this);
-        }
-        this.addFive = function() {
-            var next = this.amount() + 5;
-            this.amount(next).bind(this);
-        }
-        this.addTen = function() {
-            var next = this.amount() + 10;
-            this.amount(next).bind(this);
-        }
-        this.init = function() {
-            console.info('charity/' + params.charityId);
-            firebase.app().database().ref('charity/' + params.charityId).once('value').then(snap => {
-                var val = snap.val();
-                console.info(snap.val());
-                self.imageUrl(val.imageUrl);
-                self.siteUrl(val.siteUrl);
-                self.title(val.name);
-                self.description(val.description);
-            })
-            .then(() => self.isLoading(false));
+    self.user.subscribe(value => {
+        fb.database().ref('user/' + value.uid).on('value', snap => {
+            var pageId = snap.val().pageId;
+            console.info('Received user page id ' + pageId);
+            self.pageId(pageId);
+        })
+    })
+
+    self.init = () => {
+
+    }
+
+    self.signIn = () => {
+        var uiConfig = {
+            signInSuccessUrl: '/',
+            signInOptions: [
+              // Leave the lines as is for the providers you want to offer your users.
+              firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+              firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+              firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+              firebase.auth.GithubAuthProvider.PROVIDER_ID,
+              firebase.auth.EmailAuthProvider.PROVIDER_ID,
+              firebase.auth.PhoneAuthProvider.PROVIDER_ID
+            ],
+            // Terms of service url.
+            tosUrl: 'http://test.com'
+        };
+    
+        // Initialize the FirebaseUI Widget using Firebase.
+        var ui = new firebaseui.auth.AuthUI(fb.auth());
+        ui.start("#auth-container", uiConfig);
+    }
+}
+
+var EditPageFormVM = function (onSubmit) {
+    const self = this;
+
+    self.name = ko.observable('');
+    self.message = ko.observable('');
+    self.charities = ko.observable([]);
+
+    self.onSubmit = () => {
+        var model = {
+            name: self.name(),
+            message: self.message(),
+            charities: self.charities()
         }
 
-        this.init();
-    },
-    template: `
-        <div class="card" style="width: 18rem;">
-            <div data-bind="if: isLoading">
-                <div class="progress">
-                    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
-                </div>
-            </div>
-            <div data-bind="ifnot: isLoading">
-                <div class="card-img-top">
-                    <a data-bind="attr: {href: siteUrl}" href="">
-                        <img data-bind="attr: {src: imageUrl}" class="card-img-top" src="..." alt="Card image cap">
-                    </a>
-                </div>
-                <div class="card-body">
-                    <h5 data-bind="text: title" class="card-title"></h5>
-                    <p data-bind="text: description" class="card-text"></p>
-                </div>
-                <div class="card-footer text-muted">
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <button class="btn btn-secondary" type="button">-$5</button>
-                        </div>
-                        <input data-bind="value: amount" type="number" class="form-control text-center" aria-label="Amount (to the nearest dollar)">
-                        <div class="input-group-append">
-                            <span class="input-group-text" id="basic-addon2">$</span>
-                            <button class="btn btn-secondary" type="button">+$5</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>`
-})
+        onSubmit(model);
+    }
+}
+
+var EditPageVM = function(fb, pageId) {
+    if (!fb) {
+        throw new Error('Firebase not initilized.');
+    }
+
+    const self = this;
+    self.data = ko.observable({});
+    self.onSubmit = (model) => {
+        fb.database().ref('page/')
+    }
+    self.page = new EditPageFormVM();
+
+    userObservable.subscribe(value => {
+        fb.database().ref('user/' + value.uid).on('value', snap => {
+            self.data(snap.val());
+        })
+    })
+
+}
+
+var HomeVM = function(fb) {
+    if (!fb) {
+        throw new Error('Firebase not initilized.');
+    }
+
+    const self = this;
+    
+    self.auth = new AuthVM(fb);
+    self.page = new EditPageVM(fb, self.auth.pageId);
+
+    self.init = () => {
+        self.auth.init();
+    }
+}
+
+const launchHome = (fb) => {
+    var vm = new HomeVM(fb);
+    vm.init();
+    ko.applyBindings(vm);
+}
